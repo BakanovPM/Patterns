6df360b72c7865bb0618384280ecf9cdc288a556
pashtetrus33 <testerrus33@gmail.com>
2023-02-03 22:38:54 +0300

    Observer

---
+++ src/Observer/Subscriber1.java
@@ -0,0 +1,8 @@
+package Observer;
+
+public class Subscriber1 implements PublisherActionListener{
+    @Override
+    public void doAction(String message) {
+        System.out.println(message + " from " + this.getClass().getName());
+    }
+}
---
+++ src/Observer/Subscriber2.java
@@ -0,0 +1,8 @@
+package Observer;
+
+public class Subscriber2 implements PublisherActionListener{
+    @Override
+    public void doAction(String message) {
+        System.out.println(message + " from " + this.getClass().getName());
+    }
+}
